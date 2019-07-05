51770963fee2ba9e67ddbf5d735dbebd89acc000
ehdhkswn1997 <ehdhkswn@naver.com>
2019-07-06 04:30:48 +0900

    final

--- team3/settings.py
+++ team3/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3+*l$paga!mw=hzqz+sw%2*bl+&4!f
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
